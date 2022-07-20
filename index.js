526c8f6c7c3b44ece44c73b390d7830f9ed28a06
Tomas Makaj <Tmakajcoding@gmail.com>
2022-07-20 11:43:45 -0400

    commit tree 713542b695f9ab2961bcadcfc32786d2de257ed0

--- index.js
+++ index.js
@@ -28,14 +28,15 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split (' ');
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
+const [bessie, ,dolly, babe, little] = farmAnimals.split (' ');
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
+const [blackAndWhite, , black, pink,] = farmAnimals.split (' ')
 // Arrays
-
+const [red, orange, yellow, green, blue, indigo, violet] = colors
 // 4. Use destructuring to assign appropriate variables using the color names.
+const [r, o, y, g, b, indg, v] = colors
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
@@ -44,5 +45,17 @@ const nestedMuppet = {
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+const {muppetName, color, song, job, partner} = muppet
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const {song_2, song_4} = nestedName.album.theMuppetmMovie
+const {k_job, k_partner} = muppet
+  
+    // expect(song2).to.exist
+    // expect(song2).to.equal("Moving Right Along")
+    // expect(song4).to.exist
+    // expect(song4).to.equal("I Hope That Something Better Comes Along")
+    // expect(nestedJob).to.exist
+    // expect(nestedJob).to.equal("Host of The Muppet Show")
+    // expect(nestedPartner).to.exist
+    // expect(nestedPartner).to.equal("Miss Piggy")
